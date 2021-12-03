cc3a0aa7cbed5d8b3311542d78b43b09e2166f66
Karl Barrus <klbarrus@yahoo.com>
2021-12-02 19:26:30 -0500

    day 02 - refactor

--- aoc-02/src/main.rs
+++ aoc-02/src/main.rs
@@ -8,39 +8,34 @@ fn main() -> io::Result<()> {
     let lines = aoclib::read_file_lines(&"aoc-02.txt")?;
 
     let prog_data = lines
-    .iter()
-    .flat_map(|l| l.split(" "))
-    .tuples()
-    .map(|(a,b)| (a, b.parse::<i32>().unwrap()))
-    .collect::<Vec<(_,_)>>();
+        .iter()
+        .flat_map(|l| l.split(" "))
+        .tuples()
+        .map(|(a, b)| (a, b.parse::<i32>().unwrap()))
+        .collect::<Vec<(_, _)>>();
 
     //println!("{:#?}", prog_data);
 
-    let mut hpos = 0i32;
-    let mut depth = 0i32;
-    for &(inst, x) in prog_data.iter() {
-        match inst {
-            "forward" => { hpos += x; }
-            "up" => { depth -= x; }
-            "down" => { depth += x; }
-            _ => { println!("unexpected command"); }
-        }
-    }
+    let (hpos, depth) = prog_data
+        .iter()
+        .fold((0, 0), |(h, d), (op, x)| match op {
+            &"forward" => (h + x, d),
+            &"up" => (h, d - x),
+            &"down" => (h, d + x),
+        _ => (h, d),
+    });
 
     let part1 = hpos * depth;
     println!("Part 1: {}", part1);
 
-    let mut hpos = 0i32;
-    let mut aim = 0i32;
-    let mut depth = 0i32;
-    for &(inst, x) in prog_data.iter() {
-        match inst {
-            "forward" => { hpos += x; depth += aim * x; }
-            "up" => { aim -= x; }
-            "down" => { aim += x; }
-            _ => { println!("unexpected command"); }
-        }
-    }
+    let (hpos, depth, _) = prog_data
+        .iter()
+        .fold((0, 0, 0), |(h, d, a), (op, x)| match op {
+            &"forward" => (h + x, d + a * x, a),
+            &"up" => (h, d, a - x),
+            &"down" => (h, d, a + x),
+            _ => (h, d, a),
+        });
 
     let part2 = hpos * depth;
     println!("Part 2: {}", part2);
